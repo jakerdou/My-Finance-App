542009b9c6445336f6a7fb711f0b722b956115c1
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-01-19 14:41:10 -0600

    Update TransCategory.py

--- TransCategory.py
+++ TransCategory.py
@@ -1,7 +1,15 @@
 class TransCategory:
     name = ""
     isExpense = True
+    catTransList = []
 
     def __init__(self):
         name = ""
         isExpense = True
+        catTransList = []
+
+    def printCat(self):
+        print("Name: " + self.name)
+        print("Number of transactions: " + str(len(self.catTransList)))
+
+    #FIXME: thinking we should maybe add transactions through this class
