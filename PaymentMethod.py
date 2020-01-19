5109734404ca2ae2d5ecd002834c3e90fe1625f1
James Robinson <44409757+jakerdou@users.noreply.github.com>
2020-01-19 14:40:21 -0600

    Update PaymentMethod.py

--- PaymentMethod.py
+++ PaymentMethod.py
@@ -4,41 +4,38 @@ import TransCategory as transCat
 class PaymentMethod:
     name = ""
     balance = 0.0
-    transList = []
+    #transList = []
     categoryList = []
 
     def __init__(self):
         name = ""
         balance = 0.0
 
-    def AddTransaction(self, transToAdd):
-        if(not transToAdd.transCategory.isExpense):
+    def AddTransaction(self, catAddingTo, transToAdd):
+        if(not catAddingTo.isExpense):
             modifier = 1
         else:
             modifier = -1
 
-        #add to list
-        self.transList.append(transToAdd)
+        transToAdd.transCategory = catAddingTo
+
+        #add to list (pass catAddingTo by reference)
+        catAddingTo.catTransList.append(transToAdd)
 
         #add or subtract from balance
         self.balance = self.balance + modifier * transToAdd.amount
 
 
-    def RemoveTransaction(self, indexToRemove): #maybe change to where you're referencing an object and not an index
-        if(not self.transList[indexToRemove].transCategory.isExpense):
-            modifier = -1
-        else:
-            modifier = 1
-
-        #add or subtract from balance
-        self.balance = self.balance + modifier * self.transList[indexToRemove].amount
-
-        #remove from list
-        del self.transList[indexToRemove]
+    def RemoveTransaction(self, catRemovingFrom, transToRemove):
+        print("this function is not working yet")
 
 
     def AddCategory(self, categoryToAdd):
         self.categoryList.append(categoryToAdd)
 
-    def RemoveCategory(self, indexToRemove):
-        del self.categoryList[indexToRemove]
+    def RemoveCategory(self, categoryToRemove):
+        print("this function is not working yet")
+
+    def printPM(self):
+        print("Name: " + self.name)
+        print("Balance: $" + str(self.balance))
